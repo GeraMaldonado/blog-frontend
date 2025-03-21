9eaafb283446c2a693281da72413c8d4146b55d9
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-03-21 15:06:31 -0600

    add: contexto global Auth

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext, useState } from 'react'
+import { loginUser } from '../services/authService'
+
+interface User {
+  id: string
+  email: string
+  username: string
+}
+
+interface AuthContextType {
+  user: User | null
+  login: ({ email, password }: { email: string, password: string }) => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextType | null>(null)
+
+export async function AuthProvider ({ children }: { children: React.ReactNode }): Promise<React.FC> {
+  const [user, setUser] = useState<User | null>(null)
+
+  const login = async ({ email, password }: { email: string, password: string }): Promise<void> => {
+    try {
+      const userData = await loginUser({ email, password })
+      setUser(userData)
+    } catch (error) {
+      console.error('Error en login', error)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = (): AuthContextType | null => {
+  const context = useContext(AuthContext)
+  if (context == null) throw new Error('useAuth debe usarse dento de un AuthProvider')
+  return context
+}
