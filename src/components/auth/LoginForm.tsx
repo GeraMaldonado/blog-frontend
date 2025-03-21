e53d05d328f653edb67c1794c1cb5d5761c6d6d8
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-03-21 15:08:09 -0600

    add: formulario inicio de sesion

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,17 +1,30 @@
+import { useState } from 'react'
+
 export function LoginForm (): React.FC<Element> {
+  const [user, setUser] = useState(null)
+
   const handleSubmit = (event: any): void => {
     event.preventDefault()
     const form = event.target
     const formDate = new FormData(form)
     const entries = Object.fromEntries(formDate.entries())
-    console.log(entries)
+    fetch('http://localhost:3010/api/auth/login', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify(entries),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(async res => await res.json()).then(json => {
+      setUser(json)
+    }).catch(err => console.error(err))
   }
 
   return (
     <form className='loginForm' onSubmit={handleSubmit}>
-      <label>User: </label>
-      <input className='loginForm-username' name='username' required />
-      <label>Contraseña: </label>
+      <label>email: </label>
+      <input className='loginForm-username' name='email' required />
+      <label>contraseña: </label>
       <input className='loginForm-password' name='password' type='password' required />
       <button type='submit'>Iniciar sesión</button>
     </form>
