119954b72a35011fe9a947ae83469dde18a90464
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-03-30 14:11:36 -0600

    add: se agrega contexto de autentificacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { App } from './App.tsx'
+import { AuthProvider } from './contexts/AuthContext.js'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>
 )
