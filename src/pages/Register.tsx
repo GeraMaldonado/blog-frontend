b5b7d7426b0129a1a747d1d87113f638d4d7e3c6
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-06 18:55:39 -0600

    add: pagina ver crear usuario

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RegisterForm } from '../components/auth/RegisterForm'
+
+export default function Register (): React.FC {
+  return (
+    <div>
+      <h2>Registro</h2>
+      <RegisterForm />
+    </div>
+  )
+}
