c47048e53b530464b2c0b4d3ed395a2ec2ede72b
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-06 18:54:56 -0600

    add: ruta para crear usuarios y autentificar el correo

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -20,3 +20,26 @@ export async function logoutUser (): Promise<void> {
   if (!res.ok) throw new Error('Error al cerrar sesión')
   console.log(res)
 }
+
+const URL = 'http://localhost:3010/api/'
+
+export async function sendVerificationCode ({ name, email }: { name: string, email: string }): Promise<void> {
+  const res = await fetch(URL + 'users/verify-request', {
+    method: 'POST',
+    body: JSON.stringify({ name, email }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  if (!res.ok) throw new Error('Error al enviar el codigo')
+  console.log(res)
+}
+
+export async function registerUser (data: { name: string, email: string, username: string, password: string, code: string }): Promise<void> {
+  const res = await fetch(URL + 'users', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  console.log(data)
+  if (!res.ok) throw new Error('Error al crear usuario')
+  console.log(res)
+}
