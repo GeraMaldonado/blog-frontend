aca205e9deb5925f026004714df7b1e0a59d3fe5
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-12 12:17:28 -0600

    add: envolviendo app en contexto rutas

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { App } from './App'
 import { AuthProvider } from './contexts/AuthContext.js'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 )
