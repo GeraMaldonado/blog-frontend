7b496e6b337cc366a6723a27b82916ad46f8ef9b
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-12 12:26:55 -0600

    add: agregando componente rutas a la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { AppRoutes } from './routes/AppRoutes'
 
 export function App (): React.FC {
   return (
     <>
       <h1>Prueba desde App</h1>
+      <AppRoutes />
     </>
   )
 }
