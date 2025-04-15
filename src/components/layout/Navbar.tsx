a5f000083e2b958a683132483632a9941ed84cd6
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-14 22:53:57 -0600

    add: creacion de barra de navegacion

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import { useAuth } from '../../contexts/AuthContext'
+import './Navbar.css'
+
+export default function Navbar (): React.FC {
+  const { user, logout } = useAuth()
+
+  const handlerLogut = async (event): Promise<void> => {
+    console.log('Logout en Nav')
+    event.preventDefault()
+    await logout()
+  }
+
+  return (
+    <nav className='navbar'>
+      <Link to='/'>Inicio</Link>
+
+      {user
+        ? (
+          <>
+            <span>Hola {user.username}</span>
+            <Link to='/create'>Crear post</Link>
+            <Link to='profile'>Perfil</Link>
+            <button onClick={handlerLogut}>Cerrar Sesión</button>
+          </>
+          )
+        : (
+          <>
+            <Link to='/login'>Iniciar Sesión</Link>
+            <Link to='/register'>Registrarse</Link>
+          </>
+          )}
+    </nav>
+  )
+}
