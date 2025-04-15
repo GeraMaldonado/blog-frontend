56bc3cc179257396ed547acb7c3723479b0ef64f
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-14 22:59:41 -0600

    add: navegador en aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { AppRoutes } from './routes/AppRoutes'
+import Navbar from './components/layout/Navbar'
 
 export function App (): React.FC {
   return (
     <>
       <h1>Prueba desde App</h1>
+      <Navbar />
       <AppRoutes />
     </>
   )
