422eec06d2d9ebd7b4dac54031b0029cd74f065e
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-15 22:52:06 -0600

    add: nueva ruta home

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { LoginForm } from '../components/auth/LoginForm'
 import Register from '../pages/Register'
 import Profile from '../pages/Profile'
+import Home from '../pages/Home'
 
 export function AppRoutes () {
   return (
@@ -9,6 +10,7 @@ export function AppRoutes () {
       <Route path='/login' element={<LoginForm />} />
       <Route path='/register' element={<Register />} />
       <Route path='/profile' element={<Profile />} />
+      <Route path='/' element={<Home />} />
     </Routes>
   )
 }
