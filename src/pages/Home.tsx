1f843800abad83a0ceb9bebf518f67f8d3cf88fb
Gera Maldonado <gmaldonadofelix@gmail.com>
2025-04-17 20:29:08 -0600

    refactor: sacando logica a services

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,10 @@
-import { PostCard } from '../components/posts/PostCard'
+import { PostList } from '../components/posts/PostList'
 
 export default function Home () {
-  const ejemplos = [{
-    id: '1',
-    title: 'Mi primer post',
-    author: 'Gerardo',
-    createdAt: '2025-04-20T12:00:00Z',
-    summary: 'Este es un ejemplo de contenido de un post que será recortado para vista previa.'
-  }, {
-    id: '2',
-    title: 'Otro tema interesante',
-    author: 'Gerardo',
-    createdAt: '2025-04-19T10:00:00Z',
-    summary: 'Contenido más largo para ver cómo se comporta el componente PostCard...'
-  }]
   return (
     <div>
       <h1>Post recientes</h1>
-      {ejemplos.map(post => (
-        <PostCard key={post.id} {...post} />
-      ))}
+      <PostList />
     </div>
   )
 }
